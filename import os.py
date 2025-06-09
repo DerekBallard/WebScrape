e96669eb2854e2662b255eecf8ee1d15e4743af5
Derek Ballard <156229302+DerekBallard@users.noreply.github.com>
2025-06-09 11:22:30 -0400

    Delete import os.py

--- import os.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def send_notification(title, message):
-    os.system(f'''osascript -e 'display notification "{message}" with title "{title}"' ''')
-
-# Test it
-send_notification("VS Code Test", "This is a test alert while running Python in VS Code.")
